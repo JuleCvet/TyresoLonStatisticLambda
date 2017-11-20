3e75b5aafd6820cfc3d92f6885e9a79c6b5b88ce
Julijana Cvetkovska <julijana_cvetkovska@yahoo.com>
2017-11-20 15:16:26 +0100

    lon specifition

---
+++ src/tyresojava2lonestatisticcollections/TyresoJava2LoneStatisticCollections.java
@@ -0,0 +1,77 @@
+
+package tyresojava2lonestatisticcollections;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TyresoJava2LoneStatisticCollections {
+
+    public static void main(String[] args) {
+
+ Map<String,Integer> list = new HashMap<String,Integer>();
+ 
+    System.out.println("Lonestatic baserad pa Sektor: ");
+    list.put("Samtliga sektorer ", 41800);
+    list.put("Statlig sektor ", 38600);
+    list.put("Kommunal sektor ", 36000);
+    list.put("Landstigssektor ", 37600);
+    list.put("Privat sektor ", 42300);
+    list.put("Privatanstalda tjanstaman ", 42300);
+    
+ 
+		int sum = 0;
+
+		for (Map.Entry<String, Integer> l : list.entrySet()) {
+                    String key = l.getKey();
+                    System.out.println(key + ", " + l.getValue());
+                    sum +=  l.getValue(); 
+		}
+		System.out.println("\nGenomsnittslöner är: " + sum / list.size());
+  
+                
+                
+Map<Integer, String> treeMap = new TreeMap<Integer, String>(new Comparator<Integer>() {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+	return o2.compareTo(o1);//according to the natural ordering of its keys
+    }
+});
+        System.out.println("\nTreeMap comparation " + list);
+        
+
+ //Map<String,Integer> list = new HashMap<String,Integer>();        
+List<Map.Entry<String, Integer>> newlist = new LinkedList<>(list.entrySet());
+//will iterate in the order in which the entries were put into the map
+
+Collections.sort(newlist, new Comparator<Map.Entry<String, Integer>>() {
+    public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+	return (o1.getValue()).compareTo(o2.getValue());
+    }
+});
+        System.out.println("\nSort with LinkedList "+list);
+        
+        
+        System.out.println("\nI stigande ordning: ");
+		Map<String, Integer> result = new LinkedHashMap<String, Integer>();
+		for (Map.Entry<String, Integer> entry : newlist) {
+			result.put(entry.getKey(), entry.getValue());
+		}
+		for (Map.Entry<String, Integer> l : result.entrySet()) {
+				System.out.println(l);		
+	}
+                
+
+                System.out.println("\nSektorerna som tjänar mera än 40000 kr: " );
+                	for (Map.Entry<String, Integer> l : result.entrySet()) {
+                            if(l.getValue()>40000)
+				System.out.println(l);
+			
+	}
+    }
+}
